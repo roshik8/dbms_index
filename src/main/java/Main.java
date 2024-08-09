d64c12ec76e0b1529298047df62acf052617ce34
roshik8 <roshik8@gmail.com>
2024-08-09 15:08:15 +0500

    индекс

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,21 @@
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        int rows_on_block = 5;
+        int rows_on_block = 100;
         var songs = CsvUtils.getSongs();
-        long start = System.currentTimeMillis();
-        SpotifySong s = songs.stream().filter(spotifySong -> spotifySong.id.equals(789455)).toList().get(0);
-        System.out.println(s);
-        System.out.println("Время поиска без индекса: " + (System.currentTimeMillis() - start));
+
 
         BPlusTree<Integer, SpotifySong> bPlusTree = new BPlusTree<>(rows_on_block);
         BTreeIndex<Integer, SpotifySong> index = new BTreeIndex<>(bPlusTree);
         index.createIndex(songs, SpotifySong.class, "id");
+
+        long start = System.currentTimeMillis();
+        SpotifySong s = songs.stream().filter(spotifySong -> spotifySong.id.equals(789455)).toList().get(0);
+        System.out.println(s);
+        System.out.println("Время поиска без индекса: " + (System.currentTimeMillis() - start));
         start = System.currentTimeMillis();
         s = index.indexScan(789455);
         System.out.println(s);
@@ -37,5 +40,28 @@ public class Main {
         index.delete(1473395);
         System.out.println("Максимальный элемент в индексе: " + index.getMax());
 
+        randomSearch(5000, songs);
+        randomSearchIndex(5000, index);
+
+    }
+
+    private static void randomSearchIndex(int n, BTreeIndex<Integer, SpotifySong> index) {
+        Random random = new Random();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            var s = index.indexScan(random.nextInt(1000000));
+            //System.out.println(s);
+        }
+        System.out.println("Время поиска случайных " + n + " элементов по индексу " + (System.currentTimeMillis() - start));
+    }
+
+    private static void randomSearch(int n, List<SpotifySong> table) {
+        Random random = new Random();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            var s = table.stream().filter(spotifySong -> spotifySong.id.equals(random.nextInt(1000000))).toList();
+            //System.out.println(s);
+        }
+        System.out.println("Время поиска случайных " + n + " элементов без индекса " + (System.currentTimeMillis() - start));
     }
 }
